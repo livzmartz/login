dc2afec51dd89c3d6e7db024a7ccdc465301abd1
Livni <llmc0383@gmail.com>
2023-08-16 08:20:22 -0600

    se modifico el layout para el inicio

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# DEMO MINDEF MVC
-Base para programación MVC con php del Ministerio de la Defensa
+# LOGIN
+Formulario de para el ingreso
--- views/pages/index.php
+++ views/pages/index.php
@@ -1,11 +1,11 @@
 <div class="row mb-3">
   <div class="col text-center">
-    <h1>Ejemplo</h1>
+    <h1>Ingreso a la Aplicación</h1>
   </div>
 </div>
 <div class="row justify-content-center">
   <div class="col-lg-4">
-    <img src="./images/cit.png" width="100%" alt="">
+    <center> <img src="./images/cit.png" width="35%" alt=""></center>
   </div>
 </div>
 <script src="build/js/inicio.js"></script>
