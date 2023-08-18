15484976f5cf7f017aa6fea290020214ab7f453f
Livni <llmc0383@gmail.com>
2023-08-17 19:48:45 -0600

    se agrego la funcion de session_destroy, para cerrar la sesion

--- controllers/MenuController.php
+++ controllers/MenuController.php
@@ -11,4 +11,28 @@ class MenuController {
         $router->render('menu/index', []);
     }
 
+    public static function cerrarsesion() {
+
+        $sesion= session_destroy();
+ 
+        echo json_encode($sesion);
+        exit;
+ 
+         if($session== true){
+ 
+             echo json_encode([
+                 'codigo'=> '1'
+             ]);
+             exit;
+         }
+         else{
+ 
+             echo json_encode([
+ 
+                 'codigo'=>'2'
+             ]);
+             exit;
+        }
+    }
+ 
 }
